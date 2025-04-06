39b38193d2081e245f0d72012e41f2bf5dd1a0af
Van Dao <dao28901@gmail.com>
2025-04-06 22:35:48 +0700

    chinh sua lai duong dan

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,10 @@
 let apiRoot = "";
 if (process.env.BUILD_MODE === "dev") {
   apiRoot = "http://localhost:8080";
-}
-if (process.env.BUILD_MODE === "production") {
+} else {
   apiRoot = "https://be-5kst.onrender.com";
 }
+// if (process.env.BUILD_MODE === "production") {
+// }
 console.log(apiRoot);
-export const api_root = "https://be-5kst.onrender.com";
+export const api_root = apiRoot;
